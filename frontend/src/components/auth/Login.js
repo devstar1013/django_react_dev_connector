cdd6875eb9daca9a100e17ed4d87143d1464265c
devstar1013 <takash@mamchain.com>
2023-05-30 22:16:30 -0400

    phase: comp Login

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -30,15 +30,15 @@ const Register = () => {
 
   return (
     <React.Fragment>
-      <h1 className="large text-primary">Sign In</h1>
+      <h1 className="large  text-primary">Sign In</h1>
       <p className="lead">
-        <i className="fas fa-user"></i> Sign Into Your Account
+        <i className="fas fa-user"></i> Sign In Your Account
       </p>
       <form className="form" onSubmit={onSubmit}>
         <div className="form-group">
           <input
             type="email"
-            placeholder="Email Address"
+            placeholder="Email  Address"
             name="email"
             value={email}
             onChange={onChange}
@@ -54,7 +54,7 @@ const Register = () => {
             onChange={onChange}
           />
         </div>
-        <input type="submit" className="btn btn-primary" value="Login" />
+        <input type="submit" className="btn  btn-primary" value="Login" />
       </form>
       <p className="my-1">
         Don't have an account? <Link to="/register">Sign Up</Link>
